d59f49db1a58ea1441980afb68fb450911f2e2cc
ninjafakexxx-byte <ninjafakexxx@gmail.com>
2026-05-21 02:15:10 -0300

    online

--- src/core/providers/AppProviders.tsx
+++ src/core/providers/AppProviders.tsx
@@ -1,9 +1,16 @@
 import type { ReactNode } from "react";
 
+import { OnlinePresence } from "@/components/system/online-presence";
+
 type AppProvidersProps = {
   children: ReactNode;
 };
 
 export function AppProviders({ children }: AppProvidersProps) {
-  return children;
+  return (
+    <>
+      <OnlinePresence />
+      {children}
+    </>
+  );
 }
